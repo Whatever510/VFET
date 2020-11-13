d96e96966769378a9cf4b76da1d6804d9ce28288
Adrian <adrian@ries-ab.de>
2020-11-13 16:29:31 +0100

    remake

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import cv2
 import numpy
-import tkinter
+import argparse
 
 
 
-class FrameExtraction:
+print("Hello World")
 
-    def __init__(self,window,window_title):
-        self.window = window
-        self.window_title = window_title
-
-        self.window.mainloop()
 
